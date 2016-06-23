9e767a6ddcd90d1d57815b01d891260982ca5d02
Ville Takala <ville.takala@n2.fi>
2016-06-23 08:42:05 +0000

    no react

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ module.exports = {
     'es6': true
   },
 
-  'plugins': ['react'],
-
   'parserOptions': {
       'ecmaVersion': 6,
       'sourceType': 'module',
@@ -674,22 +672,6 @@ module.exports = {
 
       'no-var': 2, // require let or const instead of var (off by default)
       'generator-star-spacing': [2, 'before'], // enforce the spacing around the * in generator functions (off by default)
-
-      // React rules
-      'jsx-quotes': [2, 'prefer-single'],
-      'react/jsx-boolean-value': [2, 'always'],
-      'react/jsx-closing-bracket-location': [2, {selfClosing: 'after-props', nonEmpty: 'after-props'}],
-      'react/jsx-curly-spacing': [2, 'never', {'allowMultiline': false}],
-      'react/jsx-indent': [2, 4],
-      'react/jsx-max-props-per-line': [2, {maximum: 3}],
-      'react/prefer-es6-class': [2, 'always'],
-      'react/require-render-return': 2,
-      'react/self-closing-comp': 2,
-      'react/sort-comp': 2,
-      'react/prop-types': 1,
-      'react/no-did-mount-set-state': [2, 'allow-in-func'],
-      'react/no-did-update-set-state': 2,
-      'react/no-direct-mutation-state': 2,
 
 
   }
